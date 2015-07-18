964ddcaac1ae5b8a7e2823d7f620a576ed886112
Paul Wheeler <paul@paulwheeler.com>
2015-07-17 20:03:40 -0400

    add Angular scripts

---
+++ Contact.Web/app/app.js
@@ -0,0 +1,32 @@
+﻿var app = angular.module("app", ["ui.router", "ui.bootstrap"]);
+
+app.config([
+    "$locationProvider",
+    function ($locationProvider) {
+        $locationProvider.html5Mode(true);
+    }
+]);
+
+app.config([
+    "$stateProvider", "$urlRouterProvider",
+    function ($stateProvider, $urlRouterProvider) {
+
+        var authenticate = [
+            function () { return true; }
+        ];
+
+        // If the url is ever invalid, e.g. '/asdf', then redirect to '/' aka the home state
+        $urlRouterProvider
+            .otherwise("/");
+
+        $stateProvider.state("home", {
+            url: "/",
+            templateUrl: "/app/app.html"
+        });
+
+        $stateProvider.state("login", {
+            url: "/login",
+            templateUrl: "/app/login.html"
+        });
+    }
+]);
---
+++ Contact.Web/app/login.js
@@ -0,0 +1,15 @@
+﻿angular.module("app").controller("loginCtrl", [
+    "$state", "api",
+   function ($state, api) {
+       var vm = this;
+       vm.user = {};
+
+       vm.authenticate = function() {
+           return api.login(vm.user, function () {
+               $state.go("home");
+           }, function (err) {
+               alert("Login Failed: " + err.message);
+           });
+       }
+   }
+]);
